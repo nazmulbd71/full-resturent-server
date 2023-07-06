699c81e7d53b56ceb4fafe224a0858a59d7f7094
Md Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-07-07 02:36:54 +0600

    carts added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send("resturent is open")
+  res.send("resturent is open")
 })
 
 
@@ -29,19 +29,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-  
+
     const menuCollection = client.db("fullResturentDb").collection("menu");
     const reviewCollection = client.db("fullResturentDb").collection("reviews");
+    const cartCollection = client.db("fullResturentDb").collection("carts");
 
 
-    app.get('/menu',async(req,res)=>{
-        const result = await menuCollection.find().toArray();
-        res.send(result) 
+
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result)
     })
-    app.get('/reviews',async(req,res)=>{
-        const result = await reviewCollection.find().toArray();
-        res.send(result) 
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result)
     })
+
+    // cart collection 
+
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      console.log(item)
+      const result = await cartCollection.insertOne(item)
+      res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -55,5 +67,5 @@ run().catch(console.dir);
 
 
 app.listen(port, () => {
-    console.log(`Full Resturent is sitting on port ${port}`)
+  console.log(`Full Resturent is sitting on port ${port}`)
 })
